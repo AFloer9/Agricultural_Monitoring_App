a80b601fd385cc789a0d796e8fa8f476936dbf69
19floresa <alexander.flores0920@gmail.com>
2023-02-12 22:11:43 -0800

    manager.py get the needed data from the api, processes it, and stores in a csv.
    manager.py imports the module csv to print data from an array.

--- manager.py
+++ manager.py
@@ -1,17 +1,57 @@
 import subprocess
 import json
 import requests
+import csv
 #with open('weatherData.txt', 'w') as file:
 #    subprocess.run(['scrapy','crawl', 'weather','-s','LOG_ENABLED=False'], stdout=file)
 #data  = subprocess.run('curl -X GET "https://api.weather.gov/gridpoints/PQR/122,44/forecast?units=us" -H  "accept: application/geo+json"', text=True, shell=True)#capture_output=True, shell=True)
+
 data  = requests.get("https://api.weather.gov/gridpoints/PQR/122,44/forecast?units=us")
 t = json.loads(data.text)
 
-print(type(t))
-print(t.keys())
-print(t["properties"].keys())
-print(len(t["properties"]["periods"]))
-print(type(t["properties"]["periods"][0]))
-print(data.text)
-#print(data)
-#data = json.load(data)
+def dayData(day):
+    data = []
+
+    data.append(day["name"])
+    data.append(str(day["temperature"]) + " " + day["temperatureUnit"])
+    data.append(day["windSpeed"])
+    data.append(day["shortForecast"])
+    data.append(day["detailedForecast"])
+
+    dewpoint = day['dewpoint']['value']
+    data.append(str(dewpoint) + ' C')
+
+    humidity = day['relativeHumidity']['value']
+    data.append(str(humidity) + ' %')
+    
+    return data
+
+def weekData(week):
+    weekData = []
+    
+    for i in week:
+        weekData.append(dayData(i))
+
+    return weekData
+
+data = t["properties"]["periods"]
+
+#[print('\t*' ,i, "\n") for i in weekData(data)] # print week data
+data = weekData(data)
+
+def dataToFile(filename, week):
+
+    with open(filename + '.csv', 'w') as file:
+        csv_write = csv.writer(file, delimiter='\t')
+
+        for i in week:
+            #length = len(i)
+            csv_write.writerow(i)
+    # api
+
+
+    # scrapy
+
+    # 
+
+dataToFile("api", data)
