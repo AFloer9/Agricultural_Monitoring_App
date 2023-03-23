91bfb288315f545260df3baa1b1745007c5fd5af
19floresa <alexander.flores0920@gmail.com>
2023-03-22 21:13:08 -0700

    These file use the open weather api's (geography and 5-day forecast)
    The file geo_open_weather_api.py returns the latitude and longitude
    of a location (a zip code or a city, state, country location).
    
    The file open_weather_api.py returns the 5-day forecast (40 data points;
    3-hour intervals) using a latitude and longitude.
    
    The file tools.py has function to conver unix time to datetime, and vice
    versa, and a function to generate attributes for each api call.

---
+++ api/tools.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def gen_attribute(variable, data):
+    return f"&{variable}={data}"
+
+def unix_to_datetime(t):
+    return datetime.fromtimestamp(t)#.date() # utcfromtimestamp(t)
+
+def datetime_to_unix(t):
+    return t.timestamp()
+
+#o = unix_to_datetime(1679378400)
+#print(o)
+#o = o.timestamp()#datetime_to_unix(0)
+#print(o)
+
+
