45952178d21ae2b7738e6599583e69c81784761e
19floresa <alexander.flores0920@gmail.com>
2023-04-01 22:00:40 -0700

    Moved weather.gov methods to api/weather_gov_api.py

--- manager.py
+++ manager.py
@@ -1,43 +1,6 @@
 import subprocess
-import json
-import requests
 import csv
 import os
-
-def dayData(day):
-    data = []
-
-    data.append(day["name"])
-    data.append(str(day["temperature"]) + " " + day["temperatureUnit"])
-    data.append(day["windSpeed"])
-    data.append(day["shortForecast"])
-    data.append(day["detailedForecast"])
-
-    dewpoint = day['dewpoint']['value']
-    data.append(str(dewpoint) + ' C')
-
-    humidity = day['relativeHumidity']['value']
-    data.append(str(humidity) + ' %')
-    
-    return data
-
-
-def weekData(week):
-    weekData = []
-    
-    for i in week:
-        weekData.append(dayData(i))
-
-    return weekData
-
-
-def dataToFile(filename, week):
-
-    with open(filename + '.csv', 'w') as file:
-        csv_write = csv.writer(file, delimiter='\t')
-
-        for i in week:
-            csv_write.writerow(i)
 
 def weatherToCSV(filename):
     with open(filename +'.txt', 'r') as file:
@@ -47,19 +10,7 @@ def weatherToCSV(filename):
                 csv_write.writerow(line[1:-2].replace("'", '').split(','))
 
 
-def manager():
-    # run each program
-    
-    # api
-    req  = requests.get("https://api.weather.gov/gridpoints/PQR/122,44/forecast?units=us")
-    dic = json.loads(req.text)
-    
-    data = dic["properties"]["periods"] # get weather data
-    
-    #[print('\t*' ,i, "\n") for i in weekData(data)] # print week data
-    data = weekData(data)
-    dataToFile("api", data)
-
+def scrapy_data():
     # scrapy
     filename = 'weatherData'
     with open(filename + '.txt', 'w') as file:
@@ -67,9 +18,10 @@ def manager():
         weatherToCSV(filename)
 
         os.remove(filename + '.txt')
-    
-    # arduino
 
 
-manager()
+def arduino_data():
+    pass
 
+
+scrapy_data()
