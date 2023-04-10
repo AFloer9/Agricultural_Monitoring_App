00fd9e8f28996670292482fc498c4fb21a13133b
AFloer9 <123134276+AFloer9@users.noreply.github.com>
2023-04-09 23:43:01 -0700

    Database tables and set up; sqlalchmodels.py, dbsetup.py
    The table are different from from the tables in the directory app/sqlalchmodels.py, but the dbsetup.py is similar to app/dbsetup.py.

---
+++ arduino/sqlalchmodels.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from dbsetup import Base
+
+
+class Sensor(Base):
+	__tablename__ = 'my_sensors'
+	ID = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+	sensor_type = Column(String, nullable=False)
+
+	def __repr__(self):
+		return f"{self.ID} {self.sensor_type}"
+
+class SensorRelation(Base):
+	__tablename__ = 'sensor_relation'
+	sensor_id = Column(Integer, ForeignKey('my_sensors.ID'), primary_key=True, nullable=False)
+	data_id = Column(Integer, ForeignKey('sensor_data.data_id'), primary_key=True, nullable=False)
+
+	def __repr__(self):
+		return f"{self.sensor_id} {self.data_id}"
+
+class SensorData(Base):
+	__tablename__ = 'sensor_data'
+	# date
+	data_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+	data = Column(Float)
+	sensor_loc = Column(String)
