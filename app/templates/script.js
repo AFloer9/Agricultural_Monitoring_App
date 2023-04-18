cb3943b54b7e12c598cdd7d0613686a25374b48d
Anna Hyer <123134038+AHyer@users.noreply.github.com>
2023-04-17 20:26:52 -0700

    front end last chance update

--- app/templates/script.js
+++ app/templates/script.js
@@ -1,7 +1,7 @@
 // Author: Anna Hyer Spring 2023 Class: Fundamentals of Software Engineering
 
-const XHreq = new XMLHttpRequest(); //make new request object
-const url = 'https://127.0.0.1:8000/seedvault'; //
+//const XHreq = new XMLHttpRequest(); //make new request object
+//const url = 'https://127.0.0.1:5500/seedvault'; //
 
 //user buttons:
 const button1 = document.querySelector("#button1");
@@ -12,35 +12,35 @@ const button5 = document.querySelector("#button5");
 const button6 = document.querySelector("#button6");
 const button7 = document.querySelector("#button7");
 
-//button click actions:
+//button click actions/functions called when b utton is clicked:
 button1.onclick = showAccount;
 button2.onclick = showSeeds;
 button3.onclick = showPlants;
 button4.onclick = showWishlist;
 button5.onclick = showSensors;
 button6.onclick = showData;
-button7.onclick = showWeather;
+button7.onclick = appendData;
 
-function showAccount(){
-    console.log("showAccount")
-}
-
+const XHreq = new XMLHttpRequest(); //make new request object
 //display seedvault to user--iterate through JSON format for each seed? "i" less than total # of database indexes?
-//showSeeds(SEEDVAULT)
-//function showSeeds(url){
-function showSeeds(){
-    console.log("showSeeds"); //debug
-    fetch("https://127.0.0.1:8000/seedvault", { //first method
+
+//"blocked by CORS policy: Response to preflight request doesn't pass access control check: It does not have HTTP ok status."
+async function showSeeds(){
+    console.log("showingSeeds"); //debug
+    fetch("http://127.0.0.1.8000/seedvault/marigold", {    //AJAX--fetch url
         method: 'GET',
+        //method: 'HEAD',
+        //method: 'POST',
+        //mode: "no-cors",
         headers: {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            //'Content-Type': 'text/xml',
+            //'Accept': 'application/json',
         },
-        body: JSON.stringify(XHreq)  
     })    
-    .then(res => res.json())
-    .then (data => console.log(data))
-    .catch(error => console.log("ERROR"))
+    .then(response => response.json())    //return response in JSON format
+    .then (response => console.log(response))
+    .catch(error => console.log(error))
     ///////////////////////////////////////////////////////
     //SEEDVAULT.open("GET", "https://127.0.0.1:8000/seedvault");    //second method
     //SEEDVAULT.send;
@@ -57,6 +57,10 @@ function showSeeds(){
             div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
             mainContainer.appendChild(div);
         }
+}
+
+function showAccount(){
+    console.log("showAccount")
 }
 
 function showPlants(){
